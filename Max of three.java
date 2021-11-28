53b962c77fedbc1a9f87f95099e0d46a3cb9bb56
ShivaPrasadEng <86601614+ShivaPrasadEng@users.noreply.github.com>
2021-11-29 00:32:37 +0530

    Create Max of three.java

---
+++ Max of three.java
@@ -0,0 +1,28 @@
+//Max of three
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc=new Scanner(System.in);
+        //System.out.println("A, B, C");
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        int C=sc.nextInt();
+
+        if(A>=B && A>=C){
+            System.out.println(A);
+        }
+        else if(B>=C){
+            System.out.println(B);
+        }
+        else{
+            System.out.println(C);
+        } 
+        
+    }
+}
