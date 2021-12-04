0f27f18dcaa9b61b3e7b698d8d97f3cbe4c857af
ShivaPrasadEng <86601614+ShivaPrasadEng@users.noreply.github.com>
2021-12-04 21:24:49 +0530

    Create Swap Two Numbers.java

---
+++ Swap Two Numbers.java
@@ -0,0 +1,19 @@
+//Swap Two Numbers
+import java.lang.*;
+import java.util.*;
+
+//Electricity bill
+public class javapr1 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		int C;
+		System.out.print(A+" "+B);
+		C=A;
+		A=B;
+		B=C;
+		System.out.print(" After : "+A+" "+B);
+        }
+	}
+
