4df9d44a352159a24f27c7a3d8feadbe458e32c9
ShivaPrasadEng <86601614+ShivaPrasadEng@users.noreply.github.com>
2021-12-04 21:19:32 +0530

    Create Sum of Digits of Numbers.java

---
+++ Sum of Digits of Numbers.java
@@ -0,0 +1,24 @@
+// Sum of Digits of numbers
+import java.lang.*;
+import java.util.*;
+
+//Electricity bill
+public class javapr1 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int c=1;
+		int sum=0;
+		while(c<=N) {
+			int N2=sc.nextInt();
+			 sum=sum+N2;
+			c=c+1;
+		}
+		  System.out.println(sum);
+        }
+	}
+
+/*
+Sum of degits
+
+*/
