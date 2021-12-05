ca1de0400841c6ca127dbf19b4374d0f8bacec66
ShivaPrasadEng <86601614+ShivaPrasadEng@users.noreply.github.com>
2021-12-05 12:00:34 +0530

    Create Continue.java

---
+++ Continue.java
@@ -0,0 +1,18 @@
+//Continue stmt
+import java.lang.*;
+import java.util.*;
+
+
+public class sp_02 {
+	public static void main(String[] args) {
+		
+		for(int n=1;n<=10;n++){
+			System.out.println(n);
+			if(n==6) {
+				n=n+1;
+				continue;
+			}
+		}
+		
+	}
+	}
