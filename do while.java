b8846c0a52f7def3a492d14105076f4c7efc95cd
ShivaPrasadEng <86601614+ShivaPrasadEng@users.noreply.github.com>
2021-12-05 13:24:54 +0530

    Create do while.java

---
+++ do while.java
@@ -0,0 +1,18 @@
+//do while
+import java.lang.*;
+import java.util.*;
+
+
+public class sp_02 {
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int A=sc.nextInt();
+		int x=1;
+		do {
+			System.out.print(x);
+			x=x+1;
+		}
+		while(x<=1);
+	}
+	}
