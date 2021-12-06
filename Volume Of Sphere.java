f0f9e850384b6857e7c31e6f2592106dbdb17c55
ShivaPrasadEng <86601614+ShivaPrasadEng@users.noreply.github.com>
2021-12-06 19:15:02 +0530

    Create Volume Of Sphere.java

---
+++ Volume Of Sphere.java
@@ -0,0 +1,9 @@
+//Volume Of Sphere
+import java.lang.Math;
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int solve(final int A) {
+        return (int)Math.ceil((4*Math.PI*A*A*A)/3);
+    }
+
+}
