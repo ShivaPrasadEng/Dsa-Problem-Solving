191a4ce59a66bd6c6bb2848071289711703e621a
ShivaPrasadEng <86601614+ShivaPrasadEng@users.noreply.github.com>
2021-12-06 19:02:18 +0530

    Create Area of Circle.java

---
+++ Area of Circle.java
@@ -0,0 +1,9 @@
+//Area of Circle
+import java.lang.Math;
+public class Solution {
+    public int solve(int A) {
+        return (int) Math.ceil(Math.PI*A*A);
+    }
+}
+//Area of Circle
+//(π * R2).
